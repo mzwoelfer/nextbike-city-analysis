c1018c76da2f26a90eb91f9a8f00de3106eb522e
mzwoelfer <>
2024-12-21 16:05:50 +0100

    Add utils functions to save and create folder

---
+++ nextbike_processing/utils.py
@@ -0,0 +1,23 @@
+import os
+import json
+import pandas as pd
+
+
+def save_json(file_path, data):
+    """
+    Save JSON data to configured path
+    """
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def save_csv(file_path, df):
+    """
+    Save Dataframe to CSV file
+    """
+    df.to_csv(file_path, index=False)
+
+
+def ensure_directory_exists(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
