c4fca741067d39205fa8ef3b38cd4231cdf24447
mzwoelfer <>
2024-12-21 23:49:52 +0100

    move trips to own module. import state form file

--- visualization/scripts/trips.js
+++ visualization/scripts/trips.js
@@ -1,16 +1,4 @@
-const state = {
-    city_id: 467,
-    city_lat: 0,
-    city_lng: 0,
-    tripsData: [],
-    activeRoutes: {},
-    stationData: [],
-    stationMarkers: {},
-    markerMap: {},
-    isPlaying: false,
-    timer: null,
-    currentTimeMinutes: 0,
-};
+import state from './state.js';
 
 let map;
 let updateThrottle;
