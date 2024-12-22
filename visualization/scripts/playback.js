8c81d33429c8f92f98d3c4b9b1ec39b0dc207b86
mzwoelfer <>
2024-12-22 18:26:03 +0100

    Formatting index.html and playback.js

--- visualization/scripts/playback.js
+++ visualization/scripts/playback.js
@@ -2,32 +2,32 @@ import state from './state.js';
 import { formatTime } from './utils.js';
 import { updateAllComponents } from './trips.js';
 
-export function updatePlayButtonUI(){
-    const playButton = document.getElementById('play-button'); 
+export function updatePlayButtonUI() {
+    const playButton = document.getElementById('play-button');
     playButton.textContent = state.isPlaying ? 'Pause' : 'Play';
 }
 
-export function updateSlider(){
+export function updateSlider() {
     document.getElementById('time-slider').value = state.currentTimeMinutes;
     document.getElementById('time-display').textContent = `${formatTime(state.currentTimeMinutes)}`;
 }
 
 
-export function togglePlay(){
-    if(state.isPlaying){
+export function togglePlay() {
+    if (state.isPlaying) {
         stopPlayback();
     } else {
-        startPlayback(); 
+        startPlayback();
     }
 }
 
-export function stopPlayback(){
+export function stopPlayback() {
     clearInterval(state.timer);
     state.isPlaying = false;
     updatePlayButtonUI();
 }
 
-export function startPlayback(){
+export function startPlayback() {
     const playback_interval_milliseconds = 100;
     const maxTime = parseInt(document.getElementById('time-slider').max, 10);
 
