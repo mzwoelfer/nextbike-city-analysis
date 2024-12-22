1da53ec9f693c02bf5972cce440db8695a3dd40a
mzwoelfer <>
2024-12-22 13:19:23 +0100

    Seemless zoom

--- visualization/scripts/trips.js
+++ visualization/scripts/trips.js
@@ -9,7 +9,7 @@ const initializeMap = (lat, lng) => {
         map.remove();
     }
 
-    map = L.map('map').setView([lat, lng], 12);
+    map = L.map('map', { zoomSnap: 0.2 }).setView([lat, lng], 12);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; OpenStreetMap contributors',
     }).addTo(map);
