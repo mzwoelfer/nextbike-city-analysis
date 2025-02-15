508554b227e70b63c999f1c3c6f7fbf1a59e468b
mzwoelfer <>
2025-02-16 00:35:28 +0100

    Workaround to add data/ directory in gh-pages branch

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -8,9 +8,14 @@ git pull origin master
 git checkout gh-pages
 git reset --hard master
 
+# Remove "data/" line if it exists
+sed -i '/^data\//d' .gitignore  
 echo "!data/" >> .gitignore
-git add data/
 
+git add .gitignore
+git commit -m 'Allow gh-pages branch to track data/ directory'
+
+git add data/
 git commit -m '[CI/CD] Update data for GitHub pages'
 git push origin gh-pages
 
