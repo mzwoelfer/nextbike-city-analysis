eb4fef35401a8cb74010ba3664fdd775406c2c44
mzwoelfer <>
2025-09-12 13:12:38 +0200

    Add testing for CLi

---
+++ collection/data_collection/tests/test_cli.py
@@ -0,0 +1,15 @@
+import unittest
+from query_nextbike import NextbikeCLI
+
+
+class TestNextbikeCLI(unittest.TestCase):
+    def test_single_city_id(self):
+        cli = NextbikeCLI(env_city_ids=[])
+        args = cli.parse_args(["--city-id", "467"])
+
+        expected_ids = [467]
+        self.assertEqual(args.city_ids, expected_ids)
+
+
+if __name__ == "__main__":
+    unittest.main()
