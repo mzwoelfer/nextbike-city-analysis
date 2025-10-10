a625d9d2943238b36a1d0ffc22b0af20fedb8c0f
M. <43217757+mzwoelfer@users.noreply.github.com>
2025-10-10 23:42:04 +0200

    Refactor collection (#79)
    * Rename more accurately
    
    * Break funcitonality into own functions
    
    * Add CLI stuff. print city info
    
    * extract parsing console output
    
    * Add testing for CLi
    
    * basic CLI test
    
    * Add multiple city_ids
    
    * TEST: Fall back to env variables city id
    
    * Add test for no env and no cli arguments
    
    * Add CLI class to query_nextbike script
    
    * FIx CLI class tests throw error when no city_ids
    
    * Rework CLI class with tests
    
    * fix script
    
    * Refactor database stuff into class
    
    * Using data classes instead
    
    * Add github action
    
    ---------
    
    Co-authored-by: mzwoelfer <>

--- collection/data_collection/query_nextbike.py
+++ collection/data_collection/query_nextbike.py
@@ -1,170 +1,390 @@
+from dataclasses import dataclass
 import requests
+import argparse
 import datetime
 import psycopg
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
-db_name = os.getenv("DB_NAME")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-city_ids = os.getenv("CITY_IDS", "").split(",")
+# ---------- DATA CLASSES ----------
+@dataclass
+class City:
+    city_id: int
+    city_name: str
+    timezone: str
+    latitude: float
+    longitude: float
+    set_point_bikes: int
+    available_bikes: int
+    last_updated: datetime.datetime
 
+    @classmethod
+    def from_api_data(cls, data: dict):
+        country = data.get("countries", [{}])[0]
+        city_data = country.get("cities", [{}])[0]
+        return cls(
+            city_id=city_data.get("uid", 0),
+            city_name=city_data.get("name", "Unknown"),
+            timezone=country.get("timezone", "Unknown"),
+            latitude=country.get("lat", 0),
+            longitude=country.get("lng", 0),
+            set_point_bikes=country.get("set_point_bikes", 0),
+            available_bikes=country.get("available_bikes", 0),
+            last_updated=datetime.datetime.now(),
+        )
 
-def fetch_data(city_id):
-    """
-    Fetch Nextbike data for a specific city by it's city ID.
-    Using Nextbike GPFS API v2
-    """
-    url = "https://maps.nextbike.net/maps/nextbike-live.json"
-    params = {"city": city_id}
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    return response.json()
-
-
-def get_places(data):
-    try:
-        # Access countries[0]["cities"][0]["places"] safely
-        places = data.get("countries", [{}])[0].get("cities", [{}])[0].get("places", [])
-    except (IndexError, KeyError):
-        places = []
-    return places
-
-
-def get_city_info(data):
-    try:
-        city = data.get("countries", [{}])[0]
-        city_info = {
-            "city_id": city.get("cities", [{}])[0].get("uid", 0),
-            "city_name": city.get("cities", [{}])[0].get("name", "Unknown"),
-            "timezone": city.get("timezone", "Unknown"),
-            "latitude": city.get("lat", 0),
-            "longitude": city.get("lng", 0),
-            "set_point_bikes": city.get("set_point_bikes", 0),
-            "available_bikes": city.get("available_bikes", 0),
-            "last_updated": datetime.datetime.now(),
-        }
-    except (IndexError, KeyError):
-        city_info = {}
-    return city_info
+    def as_tuple(self) -> tuple:
+        return (
+            self.city_id,
+            self.city_name,
+            self.timezone,
+            self.latitude,
+            self.longitude,
+            self.set_point_bikes,
+            self.available_bikes,
+            self.last_updated,
+        )
 
 
-def write_city_info_to_database(city_info):
-    conn_str: str = (
-        f"host={db_host} dbname={db_name} user={db_user} password={db_password}"
-    )
-    with psycopg.connect(conn_str) as conn:
-        with conn.cursor() as cur:
-            city_sql = """
-            INSERT INTO public.cities (
-                city_id, city_name, timezone, latitude, longitude, set_point_bikes, available_bikes, last_updated
+@dataclass
+class Bike:
+    bike_number: str
+    latitude: float
+    longitude: float
+    active: bool
+    state: str
+    bike_type: str
+    station_number: int
+    station_uid: int
+    last_updated: datetime.datetime
+    city_id: int
+    city_name: str
+
+    def as_tuple(self) -> tuple:
+        """Convert this Bike to a tuple"""
+        return (
+            self.bike_number,
+            self.latitude,
+            self.longitude,
+            self.active,
+            self.state,
+            self.bike_type,
+            self.station_number,
+            self.station_uid,
+            self.last_updated,
+            self.city_id,
+            self.city_name,
+        )
+
+    @classmethod
+    def bike_entries_from_place(
+        cls,
+        places: list[dict],
+        city_id: int,
+        city_name: str,
+        timestamp: datetime.datetime,
+    ):
+        bikes = []
+        for place in places:
+            for bike in place.get("bike_list", []):
+                bikes.append(
+                    cls(
+                        bike_number=bike.get("number", ""),
+                        latitude=place.get("lat", 0),
+                        longitude=place.get("lat", 0),
+                        active=bike.get("active", None),
+                        state=bike.get("state", ""),
+                        bike_type=bike.get("bike_type", ""),
+                        station_number=place.get("number", 0),
+                        station_uid=place.get("uid", 0),
+                        last_updated=timestamp,
+                        city_id=city_id,
+                        city_name=city_name,
+                    )
+                )
+        return bikes
+
+
+@dataclass
+class Station:
+    uid: int
+    latitude: float
+    longitude: float
+    name: str
+    spot: bool
+    station_number: int
+    maintenance: bool
+    terminal_type: str
+    last_updated: datetime.datetime
+    city_id: int
+    city_name: str
+
+    def as_tuple(self) -> tuple:
+        """Convert this Station to a tuple"""
+        return (
+            self.uid,
+            self.latitude,
+            self.longitude,
+            self.name,
+            self.spot,
+            self.station_number,
+            self.maintenance,
+            self.terminal_type,
+            self.last_updated,
+            self.city_id,
+            self.city_name,
+        )
+
+    @classmethod
+    def build_station_entries(
+        cls,
+        places: list[dict],
+        city_id: int,
+        city_name: str,
+        timestamp: datetime.datetime,
+    ) -> list[tuple]:
+        station_entries = []
+        for place in places:
+            if place.get("bike") is False:
+                station_entries.append(
+                    cls(
+                        uid=place.get("uid", 0),
+                        latitude=place.get("lat", 0),
+                        longitude=place.get("lng", 0),
+                        name=place.get("name", "Unknown"),
+                        spot=place.get("spot", None),
+                        station_number=place.get("number", 0),
+                        maintenance=place.get("maintenance", None),
+                        terminal_type=place.get("terminal_type", "Unknown"),
+                        last_updated=timestamp,
+                        city_id=city_id,
+                        city_name=city_name,
+                    )
+                )
+
+        return station_entries
+
+
+class NextbikeAPI:
+    """INteract with Nextbike API"""
+
+    BASE_URL = "https://maps.nextbike.net/maps/nextbike-live.json"
+
+    def __init__(self, city_id: str):
+        self.city_id = city_id
+
+    def fetch_data(self) -> dict:
+        """
+        Fetch Nextbike data for a specific city by it's city ID.
+        Using Nextbike GPFS API v2
+        """
+        params = {"city": self.city_id}
+        response = requests.get(self.BASE_URL, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def extract_places(data: dict) -> list[dict]:
+        """
+        Places in Nextbike can be stations AND bikes.
+        Some bikes might not be returned to stations.
+        Some bikes pile up at one location and make a new 'place'.
+        """
+        try:
+            # Access countries[0]["cities"][0]["places"] safely
+            places = (
+                data.get("countries", [{}])[0].get("cities", [{}])[0].get("places", [])
             )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            ON CONFLICT DO NOTHING;
-            """
-            cur.execute(city_sql, tuple(city_info.values()))
+        except (IndexError, KeyError):
+            places = []
+        return places
 
-        conn.commit()
+    @staticmethod
+    def get_city_info(data: dict) -> dict:
+        try:
+            city = data.get("countries", [{}])[0]
+            city_info = {
+                "city_id": city.get("cities", [{}])[0].get("uid", 0),
+                "city_name": city.get("cities", [{}])[0].get("name", "Unknown"),
+                "timezone": city.get("timezone", "Unknown"),
+                "latitude": city.get("lat", 0),
+                "longitude": city.get("lng", 0),
+                "set_point_bikes": city.get("set_point_bikes", 0),
+                "available_bikes": city.get("available_bikes", 0),
+                "last_updated": datetime.datetime.now(),
+            }
+        except (IndexError, KeyError):
+            city_info = {}
 
-    return
+        return city_info
 
 
-def write_to_database(bike_entries, station_entries):
-    conn_str: str = (
-        f"host={db_host} dbname={db_name} user={db_user} password={db_password}"
-    )
-    with psycopg.connect(conn_str) as conn:
-        with conn.cursor() as cur:
-            bike_sql = """
-            INSERT INTO public.bikes (
-                bike_number, latitude, longitude, active, state, bike_type, station_number, station_uid, last_updated, city_id, city_name
+# ---------- Console output ----------
+class ConsolePrinter:
+    """Print nextbike info to console"""
+
+    @staticmethod
+    def print_summary(city: City, bike_entries: list, station_entries: list):
+        print("City info:", city)
+        print(
+            f"Bike entries: {len(bike_entries)}, Station entries: {len(station_entries)}"
+        )
+        print("-" * 40)
+
+
+# ---------- CLI parser ----------
+class NextbikeCLI:
+    def __init__(self, args=None):
+        parsed = self._parse_args(args=args)
+        self.city_ids = parsed.city_ids
+        self.save = parsed.save
+
+    def _parse_args(self, args=None):
+        parser = argparse.ArgumentParser(description="Nextbike data collector CLI")
+        parser.add_argument(
+            "--city-ids",
+            type=int,
+            nargs="+",
+            default=None,
+            help="City ID(s) to fetch. Defaults to .env CITY_IDS.",
+        )
+        parser.add_argument(
+            "--save", action="store_true", help="Save fetched Nextbike data to database"
+        )
+        parsed = parser.parse_args(args)
+
+        return parsed
+
+
+class AppConfig:
+    def __init__(self, cli_city_ids=[]):
+        self.city_ids = []
+        environment_city_ids = []
+
+        load_dotenv()
+        self.db_host = os.getenv("DB_HOST")
+        self.db_port = os.getenv("DB_PORT")
+        self.db_name = os.getenv("DB_NAME")
+        self.db_user = os.getenv("DB_USER")
+        self.db_password = os.getenv("DB_PASSWORD")
+
+        env_city_ids = os.getenv("CITY_IDS", None)
+
+        if cli_city_ids:
+            self.city_ids = cli_city_ids
+        elif env_city_ids:
+            for city_id in env_city_ids.split(","):
+                environment_city_ids.append(int(city_id))
+            self.city_ids = environment_city_ids
+        else:
+            raise ValueError(
+                "No city ID provided. Use --city-ids or set CITY_IDS in .env."
             )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            ON CONFLICT DO NOTHING;
-            """
-            cur.executemany(bike_sql, bike_entries)
 
-            station_sql = """
-            INSERT INTO public.stations (
+
+# ---------- DATABASE STUFF ----------
+class PostgresClient:
+    """Handle database entries"""
+
+    def __init__(
+        self, db_host=None, db_port=None, db_name=None, db_user=None, db_password=None
+    ):
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+
+        self.connection_string = (
+            f"host={db_host} dbname={db_name} user={db_user} password={db_password}"
+        )
+
+    def insert_city_information(self, city: City):
+        city_sql = """
+        INSERT INTO public.cities (
+            city_id, city_name, timezone, latitude, longitude, set_point_bikes, available_bikes, last_updated
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT DO NOTHING;
+        """
+        with (
+            psycopg.connect(self.connection_string) as connection,
+            connection.cursor() as cursor,
+        ):
+            cursor.execute(city_sql, city.as_tuple())
+            connection.commit()
+
+    def insert_bike_entries(self, bike_entries):
+        sql_statement = """
+        INSERT INTO public.bikes (
+            bike_number, latitude, longitude, active, state, bike_type, station_number, station_uid, last_updated, city_id, city_name
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT DO NOTHING;
+        """
+
+        bikes = [bike.as_tuple() for bike in bike_entries]
+        with (
+            psycopg.connect(self.connection_string) as connection,
+            connection.cursor() as cursor,
+        ):
+            cursor.executemany(sql_statement, bikes)
+            connection.commit()
+
+    def insert_station_entries(self, station_entries: list[tuple]):
+        sql_statement = """
+        INSERT INTO public.stations (
                 uid, latitude, longitude, name, spot, station_number, maintenance, terminal_type, last_updated, city_id, city_name
             )
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             ON CONFLICT DO NOTHING;
-            """
-            cur.executemany(station_sql, station_entries)
+        """
 
-        conn.commit()
-
-    return
+        stations = [station.as_tuple() for station in station_entries]
+        with (
+            psycopg.connect(self.connection_string) as connection,
+            connection.cursor() as cursor,
+        ):
+            cursor.executemany(sql_statement, stations)
+            connection.commit()
 
 
 def main():
-    last_updated = datetime.datetime.now()
+    cli = NextbikeCLI()
+    config = AppConfig(cli.city_ids)
 
+    last_updated = datetime.datetime.now()
+    postgres_client = PostgresClient(
+        config.db_host,
+        config.db_port,
+        config.db_name,
+        config.db_user,
+        config.db_password,
+    )
+
+    city_ids = config.city_ids
     for city_id in city_ids:
         print(f"Collecting nextbike data from city: {city_id}")
+        api = NextbikeAPI(city_id)
+        data = api.fetch_data()
+        city = City.from_api_data(data)
+        places = api.extract_places(data)
 
-        data = fetch_data(city_id)
-
-        city_name = (
-            data.get("countries", [{}])[0].get("cities", [{}])[0].get("name", "Unknown")
+        bike_entries = Bike.bike_entries_from_place(
+            places, city.city_id, city.city_name, last_updated
         )
-        places = get_places(data)
+        station_entries = Station.build_station_entries(
+            places, city.city_id, city.city_name, last_updated
+        )
 
-        bike_entries = []
-        station_entries = []
+        ConsolePrinter.print_summary(city, bike_entries, station_entries)
 
-        for place in places:
-            place_uid = place.get("uid", 0)
-            place_lat = place.get("lat", 0)
-            place_lng = place.get("lng", 0)
-            place_is_bike = place.get("bike", None)
-            place_name = place.get("name", "Unknown")
-            place_spot = place.get("spot", None)
-            place_number = place.get("number", 0)
-            place_maintenance = place.get("maintenance", None)
-            place_terminal_type = place.get("terminal_type", "Unknown")
-
-            for bike in place.get("bike_list", []):
-                bike_entries.append(
-                    (
-                        bike.get("number", ""),
-                        place_lat,
-                        place_lng,
-                        bike.get("active", None),
-                        bike.get("state", ""),
-                        bike.get("bike_type", ""),
-                        place_number,
-                        place_uid,
-                        last_updated,
-                        city_id,
-                        city_name,
-                    )
-                )
-
-            if place_is_bike is False:
-                station_entries.append(
-                    (
-                        place_uid,
-                        place_lat,
-                        place_lng,
-                        place_name,
-                        place_spot,
-                        place_number,
-                        place_maintenance,
-                        place_terminal_type,
-                        last_updated,
-                        city_id,
-                        city_name,
-                    )
-                )
-        city_info = get_city_info(data)
-        write_to_database(bike_entries, station_entries)
-        write_city_info_to_database(city_info)
+        if cli.save:
+            postgres_client.insert_city_information(city)
+            postgres_client.insert_bike_entries(bike_entries)
+            postgres_client.insert_station_entries(station_entries)
+            print(f"Data saed for city {city.city_name}.")
 
 
 if __name__ == "__main__":
---
+++ collection/data_collection/tests/test_cli.py
@@ -0,0 +1,63 @@
+import os
+import unittest
+from query_nextbike import NextbikeCLI, AppConfig
+
+
+class TestNextbikeCLI(unittest.TestCase):
+    def test_single_city_id(self):
+        cli = NextbikeCLI(["--city-ids", "467"])
+
+        expected_ids = [467]
+        self.assertEqual(cli.city_ids, expected_ids)
+
+    def test_multiple_city_ids(self):
+        cli = NextbikeCLI(["--city-id", "467", "123"])
+
+        expected_ids = [467, 123]
+        self.assertEqual(cli.city_ids, expected_ids)
+
+
+class TestAppConfig(unittest.TestCase):
+    def setUp(self) -> None:
+        """Never start a test with set CITY_IDS!"""
+        os.environ["CITY_IDS"] = ""
+
+    def tearDown(self):
+        """Always remove CITY_IDS after each test"""
+        os.environ["CITY_IDS"] = ""
+
+    def test_read_env_ids(self):
+        os.environ["CITY_IDS"] = "100,200"
+        config = AppConfig()
+
+        expected_ids = [100, 200]
+        self.assertEqual(config.city_ids, expected_ids)
+
+    def test_cli_ids_take_precedence(self):
+        os.environ["CITY_IDS"] = "100,200"
+        cli_ids = [123, 456]
+        config = AppConfig(cli_city_ids=cli_ids)
+
+        self.assertEqual(config.city_ids, cli_ids)
+
+    def test_cli_city_ids(self):
+        cli_ids = [123, 456]
+        config = AppConfig(cli_city_ids=cli_ids)
+
+        self.assertEqual(config.city_ids, cli_ids)
+
+    def test_no_city_ids_raises_error_message(self):
+        os.environ["CITY_IDS"] = ""
+        with self.assertRaises(ValueError):
+            AppConfig(cli_city_ids=[])
+
+    def test_no_city_ids_return_error_message(self):
+        os.environ["CITY_IDS"] = ""
+        with self.assertRaisesRegex(
+            ValueError, "No city ID provided. Use --city-ids or set CITY_IDS in .env."
+        ):
+            AppConfig(cli_city_ids=[])
+
+
+if __name__ == "__main__":
+    unittest.main()
