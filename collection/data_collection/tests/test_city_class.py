5b5c091274d2f5d6a2fbb35df7766ef9f9b3da1f
mzwoelfer <>
2025-11-07 14:27:50 +0100

    First City class tests

---
+++ collection/data_collection/tests/test_city_class.py
@@ -0,0 +1,40 @@
+import unittest
+import datetime
+from query_nextbike import City
+
+
+class TestCityDataClass(unittest.TestCase):
+    def test_city_class_exists(self):
+        city = City(
+            city_id=1,
+            city_name="test_city",
+            timezone="some timezone",
+            latitude=50.8,
+            longitude=50.8,
+            set_point_bikes=1,
+            available_bikes=1,
+            last_updated=datetime.datetime.now(),
+        )
+        self.assertIsInstance(city, City)
+
+    def test_create_City_from_api_data(self):
+        api_data = {
+            "countries": [
+                {
+                    "lat": 47.4875,
+                    "lng": 12.0383,
+                    "timezone": "Europe/Berlin",
+                    "set_point_bikes": 134,
+                    "available_bikes": 87,
+                    "cities": [
+                        {
+                            "uid": 773,
+                            "name": "Kufstein",
+                        }
+                    ],
+                }
+            ]
+        }
+        city = City.from_api_data(api_data)
+
+        self.assertEqual(city.city_id, 773)
