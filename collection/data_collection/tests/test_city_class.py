d0ec5747ca78237a010e18ae47fea86e5294d94f
mzwoelfer <>
2025-11-07 14:28:57 +0100

    Outsource api_data to test class setUp

--- collection/data_collection/tests/test_city_class.py
+++ collection/data_collection/tests/test_city_class.py
@@ -4,21 +4,8 @@ from query_nextbike import City
 
 
 class TestCityDataClass(unittest.TestCase):
-    def test_city_class_exists(self):
-        city = City(
-            city_id=1,
-            city_name="test_city",
-            timezone="some timezone",
-            latitude=50.8,
-            longitude=50.8,
-            set_point_bikes=1,
-            available_bikes=1,
-            last_updated=datetime.datetime.now(),
-        )
-        self.assertIsInstance(city, City)
-
-    def test_create_City_from_api_data(self):
-        api_data = {
+    def setUp(self):
+        self.api_data = {
             "countries": [
                 {
                     "lat": 47.4875,
@@ -35,6 +22,21 @@ class TestCityDataClass(unittest.TestCase):
                 }
             ]
         }
-        city = City.from_api_data(api_data)
+
+    def test_city_class_exists(self):
+        city = City(
+            city_id=1,
+            city_name="test_city",
+            timezone="some timezone",
+            latitude=50.8,
+            longitude=50.8,
+            set_point_bikes=1,
+            available_bikes=1,
+            last_updated=datetime.datetime.now(),
+        )
+        self.assertIsInstance(city, City)
+
+    def test_create_City_from_api_data(self):
+        city = City.from_api_data(self.api_data)
 
         self.assertEqual(city.city_id, 773)
