651bdb1e2339492fa1e81ffea31a075d3d321a0a
mzwoelfer <>
2025-11-09 13:57:13 +0100

    Add station as tuple tests

--- collection/data_collection/tests/test_station_class.py
+++ collection/data_collection/tests/test_station_class.py
@@ -23,3 +23,39 @@ class TestStationExist(unittest.TestCase):
 
     def test_Station_exists(self):
         self.assertIsInstance(self.station, Station)
+
+    def test_tuple_length(self):
+        self.assertEqual(len(self.result), 11)
+
+    def test_tuple_uid(self):
+        self.assertEqual(self.result[0], 1001)
+
+    def test_tuple_latitude(self):
+        self.assertEqual(self.result[1], 47.1)
+
+    def test_tuple_longitude(self):
+        self.assertEqual(self.result[2], 11.2)
+
+    def test_tuple_name(self):
+        self.assertEqual(self.result[3], "Bahnhof")
+
+    def test_tuple_spot(self):
+        self.assertEqual(self.result[4], True)
+
+    def test_tuple_station_number(self):
+        self.assertEqual(self.result[5], 12345)
+
+    def test_tuple_maintenance(self):
+        self.assertEqual(self.result[6], False)
+
+    def test_tuple_terminal_type(self):
+        self.assertEqual(self.result[7], "sign")
+
+    def test_tuple_last_updated(self):
+        self.assertEqual(self.result[8], self.now)
+
+    def test_tuple_city_id(self):
+        self.assertEqual(self.result[9], 773)
+
+    def test_tuple_city_name(self):
+        self.assertEqual(self.result[10], "Kufstein")
