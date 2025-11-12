0455e708c0ce7184dde7c1bd3d77b3332d09080c
mzwoelfer <>
2025-11-12 17:34:14 +0100

    Database use dict instead of tuple

--- collection/data_collection/database/postgres.py
+++ collection/data_collection/database/postgres.py
@@ -1,30 +1,28 @@
 import psycopg
 from database.base import AbstractDatabaseClient, register_backend
 
+
 @register_backend("postgres")
 class PostgresClient(AbstractDatabaseClient):
     """Handle postgres entries"""
 
     def __init__(self, config):
-        
         self.config = config
-        self.connection_string = (
-            f"host={self.config.db_host} port={self.config.db_port} dbname={self.config.db_name} user={self.config.db_user} password={self.config.db_password}"
-        )
+        self.connection_string = f"host={self.config.db_host} port={self.config.db_port} dbname={self.config.db_name} user={self.config.db_user} password={self.config.db_password}"
 
     def insert_city_information(self, city):
         city_sql = f"""
         INSERT INTO {self.config.db_cities_table} (
             city_id, city_name, timezone, latitude, longitude, set_point_bikes, available_bikes, last_updated
         )
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        VALUES (%(city_id)s, %(city_name)s, %(timezone)s, %(latitude)s, %(longitude)s, %(set_point_bikes)s, %(available_bikes)s, %(last_updated)s)
         ON CONFLICT DO NOTHING;
         """
         with (
             psycopg.connect(self.connection_string) as connection,
             connection.cursor() as cursor,
         ):
-            cursor.execute(city_sql, city.as_tuple())
+            cursor.execute(city_sql, city.__dict__)
             connection.commit()
 
     def insert_bike_entries(self, bike_entries):
@@ -32,11 +30,11 @@ class PostgresClient(AbstractDatabaseClient):
         INSERT INTO {self.config.db_bikes_table} (
             bike_number, latitude, longitude, active, state, bike_type, station_number, station_uid, last_updated, city_id, city_name
         )
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        VALUES (%(bike_number)s, %(latitude)s, %(longitude)s, %(active)s, %(state)s, %(bike_type)s, %(station_number)s, %(station_uid)s, %(last_updated)s, %(city_id)s, %(city_name)s)
         ON CONFLICT DO NOTHING;
         """
 
-        bikes = [bike.as_tuple() for bike in bike_entries]
+        bikes = [bike.__dict__ for bike in bike_entries]
         with (
             psycopg.connect(self.connection_string) as connection,
             connection.cursor() as cursor,
@@ -49,11 +47,11 @@ class PostgresClient(AbstractDatabaseClient):
         INSERT INTO {self.config.db_stations_table} (
                 uid, latitude, longitude, name, spot, station_number, maintenance, terminal_type, last_updated, city_id, city_name
             )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            VALUES (%(uid)s, %(latitude)s, %(longitude)s, %(name)s, %(spot)s, %(station_number)s, %(maintenance)s, %(terminal_type)s, %(last_updated)s, %(city_id)s, %(city_name)s)
             ON CONFLICT DO NOTHING;
         """
 
-        stations = [station.as_tuple() for station in station_entries]
+        stations = [station.__dict__ for station in station_entries]
         with (
             psycopg.connect(self.connection_string) as connection,
             connection.cursor() as cursor,
